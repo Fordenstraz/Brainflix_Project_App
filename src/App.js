69fc4d2e269f306603d4fb757eaf7ae89e03fc72
Corey Bickford <cb6@live.ca>
2024-05-26 22:35:28 -0700

    Finish Button component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+//Assets/Styles
+import './App.scss';
+import uploadIcon from './assets/images/icons/upload.svg';
+import commentIcon from './assets/images/icons/add_comment.svg';
+
+//Components
+import Button from './components/Button/Button';
+
 export default function App() {
-	return <></>;
+	return (
+		<>
+			<Button
+				icon={commentIcon}
+				label='COMMENT'
+			/>
+			<Button
+				icon={uploadIcon}
+				label='UPLOAD'
+			/>
+		</>
+	);
 }
