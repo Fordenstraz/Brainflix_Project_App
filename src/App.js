12fae8be95aac97a3174889b959646914d9b64ad
Corey Bickford <cb6@live.ca>
2024-05-29 23:20:30 -0700

    Complete VideoPlayer component and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 //Assets/Styles
 import './App.scss';
+import { useState } from 'react';
 import avatar from './assets/images/Mohan-muruge.jpg';
+import JSONVideos from './data/videos.json';
+import videoData from './data/video-details.json';
 import commentIcon from './assets/images/icons/add_comment.svg';
 //Components
 import Header from './components/Header/Header';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 
 export default function App() {
+	//Set state with default video:
+	let [selectedVideo, setSelectedVideo] = useState(videoData[0]);
+
 	return (
 		<>
 			<Header userImage={avatar} />
+
+			<VideoPlayer video={selectedVideo} />
 		</>
 	);
 }
--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,14 +1,69 @@
 import './VideoPlayer.scss';
+import viewsIcon from '../../assets/images/icons/views.svg';
+import likesIcon from '../../assets/images/icons/likes.svg';
 
-export default function VideoPlayer({ video, image }) {
+export default function VideoPlayer(props) {
+	//Format timestamps from epoch, to 'MM/DD/YYYY':
+	const formatTimestamp = timestamp => {
+		const date = new Date(timestamp);
+		const month = String(date.getMonth() + 1);
+		const day = String(date.getDate() + 1);
+		const year = date.getFullYear();
+		return `${month}/${day}/${year}`;
+	};
+
 	return (
-		<main className='current-video'>
-			<video
-				className='current-video__player'
-				controls
-				poster={image}>
-				<source src={video} />
-			</video>
+		<main className='video'>
+			<figure className='video__current-selection'>
+				<video
+					className='video__player'
+					controls
+					poster={props.video.image}>
+					<source src={props.video.video} />
+				</video>
+			</figure>
+
+			<section className='video__info'>
+				<h1 className='video__title'>{props.video.title}</h1>
+
+				<div className='video__details'>
+					<div className='video__authorship'>
+						<div className='video__channel'>
+							By {props.video.channel}
+						</div>
+
+						<span>{formatTimestamp(props.video.timestamp)}</span>
+					</div>
+
+					<div className='video__metrics'>
+						<div>
+							<img
+								src={viewsIcon}
+								alt='an eye'
+							/>
+
+							{props.video.views}
+						</div>
+
+						<div>
+							<img
+								src={likesIcon}
+								alt='a heart'
+							/>
+
+							{props.video.likes}
+						</div>
+					</div>
+				</div>
+
+				<div className='video__description'>
+					{props.video.description}
+				</div>
+
+				<div className='video__comments'>
+					{props.video.comments.length} Comments
+				</div>
+			</section>
 		</main>
 	);
 }
