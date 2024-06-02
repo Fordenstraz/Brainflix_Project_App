73ea026e9b20f4371bb80df26bb2bb971e348e13
Corey Bickford <cb6@live.ca>
2024-06-02 16:02:00 -0700

    Add scroll to top on video selection

--- src/App.js
+++ src/App.js
@@ -2,44 +2,51 @@
 import './App.scss';
 import { useState } from 'react';
 import avatar from './assets/images/Mohan-muruge.jpg';
-import JSONVideos from './data/videos.json';
-import videoData from './data/video-details.json';
+import JSONVideoList from './data/videos.json';
+import JSONVideoData from './data/video-details.json';
 //Components
 import Comments from './components/Comments/Comments';
 import Header from './components/Header/Header';
 import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 import VideoInfo from './components/VideoInfo/VideoInfo';
 import VideoList from './components/VideoList/VideoList';
+//Utilities
+import scrollToTop from './components/utils/scrollToTop';
 
 export default function App() {
-	//Set state with default video:
-	const [selectedVideo, setSelectedVideo] = useState(0);
 	//Set state for current user:
 	const [user, setUser] = useState(avatar);
+	//Set state with default video:
+	const [selectedVideo, setSelectedVideo] = useState(JSONVideoData[0]);
 	//Set state of video list:
-	const [asideVideos, setAsideVideos] = useState(JSONVideos);
+	const [asideVideos, setAsideVideos] = useState(JSONVideoList);
+	//Set the selected video state:
+	const selectNewVideo = target => {
+		setSelectedVideo(JSONVideoData[asideVideos.indexOf(target)]);
+		scrollToTop();
+	};
 
 	return (
 		<>
 			<Header user={user} />
 
-			<VideoPlayer video={videoData[selectedVideo]} />
+			<VideoPlayer video={selectedVideo} />
 
 			<main className='main'>
 				<div className='main__video-section'>
-					<VideoInfo video={videoData[selectedVideo]} />
+					<VideoInfo video={selectedVideo} />
 
 					<Comments
 						user={user}
-						video={videoData[selectedVideo]}
+						video={selectedVideo}
 					/>
 				</div>
 
 				<aside className='main__other-videos'>
 					<VideoList
 						listData={asideVideos}
-						currentVideo={videoData[selectedVideo]}
-						setSelectedVideo={setSelectedVideo}
+						currentVideo={selectedVideo}
+						selectNewVideo={selectNewVideo}
 					/>
 				</aside>
 			</main>
