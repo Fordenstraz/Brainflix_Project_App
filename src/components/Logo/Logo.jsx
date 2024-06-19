7fe65e8632fe95d13c839d98a5517af928fe4dba
Corey Bickford <cb6@live.ca>
2024-06-19 14:55:49 -0700

    Add link from logo to Home page

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,17 @@
 //Assets/Styles
 import './Logo.scss';
 import logo from '../../assets/images/logo/BrainFlix-logo.svg';
+//Components
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
 	return (
-		<img
-			className='brainflix-logo-image'
-			src={logo}
-			alt='the Brainflix logo'
-		/>
+		<Link to='/'>
+			<img
+				className='brainflix-logo-image'
+				src={logo}
+				alt='the Brainflix logo'
+			/>
+		</Link>
 	);
 }
