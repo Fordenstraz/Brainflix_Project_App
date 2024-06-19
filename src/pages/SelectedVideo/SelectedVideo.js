fa9c9e59c3fee00af4439c29836bf450caa430d0
Corey Bickford <cb6@live.ca>
2024-06-18 18:53:55 -0700

    Add stylesheets to routes

---
+++ src/pages/SelectedVideo/SelectedVideo.js
@@ -0,0 +1,6 @@
+//Assets/Styles
+import './SelectedVideo.scss';
+
+export default function SelectedVideo() {
+	return;
+}
---
+++ src/pages/Upload/Upload.js
@@ -0,0 +1,12 @@
+//Components
+import Header from '../../components/Header/Header';
+//Assets/Styles
+import './Upload.scss';
+
+export default function Upload({ user }) {
+	return (
+		<>
+			<Header user={user} />
+		</>
+	);
+}
