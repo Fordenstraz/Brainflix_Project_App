e2cf42ed055f22a405a408feb3f148012461759b
Corey Bickford <cb6@live.ca>
2024-06-19 01:17:04 -0700

    Finish UploadVideo component and styling

--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -11,47 +11,53 @@ export default function UploadVideo() {
 			<section className='upload-section'>
 				<h1 className='upload-section__heading'>Upload Video</h1>
 
-				<figure className='upload-section__thumbnail'>
-					<figcaption className='upload-section__thumbnail-heading'>
-						VIDEO THUMBNAIL
-					</figcaption>
-					<img
-						className='upload-section__thumbnail-img'
-						src={thumbnail}
-						alt='video thumbnail'
-					/>
-				</figure>
+				<div className='upload-section__form-wrapper'>
+					<figure className='upload-section__thumbnail'>
+						<figcaption className='upload-section__thumbnail-heading'>
+							VIDEO THUMBNAIL
+						</figcaption>
+						<img
+							className='upload-section__thumbnail-img'
+							src={thumbnail}
+							alt='video thumbnail'
+						/>
+					</figure>
 
-				<form className='upload-section__form'>
-					<label
-						className='upload-section__label'
-						htmlFor='video-title'>
-						TITLE YOUR VIDEO
-					</label>
-					<input
-						className='upload-section__title-input'
-						type='text'
-						name='video-title'
-						id='video-title'
-						placeholder='Add a title to your video'
-					/>
+					<form className='upload-section__form'>
+						<label
+							className='upload-section__label'
+							htmlFor='video-title'>
+							TITLE YOUR VIDEO
+							<input
+								className='upload-section__title-input'
+								type='text'
+								name='video-title'
+								id='video-title'
+								placeholder='Add a title to your video'
+							/>
+						</label>
 
-					<label
-						className='upload-section__label'
-						htmlFor='video-description'>
-						ADD A VIDEO DESCRIPTION
-					</label>
-					<textarea
-						className='upload-section__desc-input'
-						name='video-description'
-						id='video-description'
-						placeholder='Add a description to your video'></textarea>
+						<label
+							className='upload-section__label'
+							htmlFor='video-description'>
+							ADD A VIDEO DESCRIPTION
+							<textarea
+								className='upload-section__desc-input'
+								name='video-description'
+								id='video-description'
+								rows='5'
+								placeholder='Add a description to your video'></textarea>
+						</label>
+					</form>
+				</div>
 
+				<div className='btn-wrapper'>
 					<Button
 						icon={publishIcon}
 						label='PUBLISH'
 					/>
-				</form>
+					<button className='cancel-btn'>CANCEL</button>
+				</div>
 			</section>
 		</div>
 	);
