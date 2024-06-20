7b35f08b6f6abcfcc425eaf95771820668f7e549
Corey Bickford <cb6@live.ca>
2024-06-19 23:02:40 -0700

    Seperate RenderVideo component from Page to allow multiple pages to use it

---
+++ src/components/RenderVideoPage/RenderVideoPage.jsx
@@ -0,0 +1,40 @@
+//Components
+import Comments from '../../components/Comments/Comments';
+import Header from '../../components/Header/Header';
+import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
+import VideoInfo from '../../components/VideoInfo/VideoInfo';
+import VideoList from '../../components/VideoList/VideoList';
+
+export default function RenderVideoPage({
+	user,
+	selectedVideo,
+	asideVideos,
+	selectNewVideo,
+}) {
+	return (
+		<>
+			<Header user={user} />
+
+			<VideoPlayer video={selectedVideo} />
+
+			<main className='main'>
+				<div className='main__video-section'>
+					<VideoInfo video={selectedVideo} />
+
+					<Comments
+						user={user}
+						video={selectedVideo}
+					/>
+				</div>
+
+				<aside className='main__other-videos'>
+					<VideoList
+						listData={asideVideos}
+						currentVideo={selectedVideo}
+						selectNewVideo={selectNewVideo}
+					/>
+				</aside>
+			</main>
+		</>
+	);
+}
