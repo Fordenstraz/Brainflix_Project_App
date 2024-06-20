223835904f33a83a254b5332542301d901e5d7f7
Corey Bickford <cb6@live.ca>
2024-06-19 23:00:50 -0700

    Add form handling to uploadVideo form

--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -1,15 +1,52 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 //Assets/Styles
 import './UploadVideo.scss';
 import publishIcon from '../../assets/images/icons/publish.svg';
 import thumbnail from '../../assets/images/Upload-video-preview.jpg';
 //Components
-import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 
 export default function UploadVideo() {
-	const confirmUpload = () => {
-		console.log(`Video upload succesful`);
-		alert(`Video upload succesful`);
+	const navigate = useNavigate();
+	//input state:
+	const [values, setValues] = useState({ title: '', description: '' });
+	//error state:
+	const [errors, setErrors] = useState({ title: false, description: false });
+
+	const handleInputChange = event => {
+		const { name, value } = event.target;
+		//spread values back into state, changing only the modified value:
+		setValues({
+			...values,
+			[name]: value,
+		});
+
+		//clear error state when user starts typing:
+		setErrors({
+			...errors,
+			[name]: false,
+		});
+	};
+
+	const confirmUpload = event => {
+		event.preventDefault();
+
+		//check for input values:
+		const newErrors = {
+			//'newErrors' is true if value is empty:
+			title: !values.title,
+			description: !values.description,
+		};
+
+		setErrors(newErrors);
+
+		// If no errors, continue:
+		if (!newErrors.title && !newErrors.description) {
+			console.log('Video upload successful');
+			alert('Video upload successful');
+			navigate('/');
+		}
 	};
 
 	return (
@@ -35,12 +72,23 @@ export default function UploadVideo() {
 							htmlFor='video-title'>
 							TITLE YOUR VIDEO
 							<input
-								className='upload-section__title-input'
+								className={`upload-section__title-input ${
+									errors.title
+										? 'upload-section__title-input--error'
+										: ''
+								}`}
 								type='text'
-								name='video-title'
+								name='title'
 								id='video-title'
 								placeholder='Add a title to your video'
+								onChange={handleInputChange}
+								value={values.title}
 							/>
+							{errors.title && (
+								<span className='error-message'>
+									This field is required
+								</span>
+							)}
 						</label>
 
 						<label
@@ -48,11 +96,22 @@ export default function UploadVideo() {
 							htmlFor='video-description'>
 							ADD A VIDEO DESCRIPTION
 							<textarea
-								className='upload-section__desc-input'
-								name='video-description'
+								className={`upload-section__desc-input ${
+									errors.description
+										? 'upload-section__desc-input--error'
+										: ''
+								}`}
+								name='description'
 								id='video-description'
 								rows='5'
-								placeholder='Add a description to your video'></textarea>
+								placeholder='Add a description to your video'
+								onChange={handleInputChange}
+								value={values.description}></textarea>
+							{errors.description && (
+								<span className='error-message'>
+									This field is required
+								</span>
+							)}
 						</label>
 					</form>
 				</div>
