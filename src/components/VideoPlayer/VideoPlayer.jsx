a13166c687bb66f69f840333df5df37d9e7ecbd8
Corey Bickford <cb6@live.ca>
2024-06-21 23:21:54 -0700

    Add short circuiting to several components

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -2,14 +2,16 @@
 import './VideoPlayer.scss';
 
 export default function VideoPlayer({ video }) {
+	console.log(video);
+
 	return (
 		<div className='video'>
 			<figure className='video__current-selection'>
 				<video
 					className='video__player'
 					controls
-					poster={video.image}>
-					<source src={video.video} />
+					poster={video?.image}>
+					<source src={video?.video} />
 				</video>
 			</figure>
 		</div>
